c2d2559855c06a827993cfe07595ca5fc6516db1
RyanJStories <124539761+RyanJStories@users.noreply.github.com>
2024-05-16 16:44:17 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Card Builder`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "ChTz8czhFasDMc_kTeF6GXaQFLGvf8pW3w9u_2_18TU",
+      "spaceId": "elmagomp499l"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
